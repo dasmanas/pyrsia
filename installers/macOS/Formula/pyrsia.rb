7e9f568971cbe6f146b2a2616ab30f81d9eb9296
Manas Das <manasd@jfrog.com>
2022-10-10 16:12:35 -0700

    pyrsia homebrew installer for macOS

--- installers/macOS/Formula/pyrsia.rb
+++ installers/macOS/Formula/pyrsia.rb
@@ -3,9 +3,14 @@ class Pyrsia < Formula
   homepage "https://pyrsia.io/"
   url "https://brewrepo.pyrsia.io/pyrsia-0.2.0.tar.gz"
   sha256 "7974164c0557823e91878f17af2e9adb2a9ff596347413fe09c8105f16ec7656"
+  license "Apache-2.0"
   version "0.2.0"
 
   def install
     bin.install "pyrsia"
+  end
+
+  test do
+    system "false"
   end
 end
